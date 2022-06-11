9221f05b69d7bd14d4babf0bc632bf4b73cc30b4
Nasir Ahamad <63605186+halknasir@users.noreply.github.com>
2022-06-11 03:13:00 +0000

    Add files via upload

---
+++ 1st project.py
@@ -0,0 +1,9 @@
+weight=int(input("weight: "))
+unit=input("(k)g or (L)bs: ")
+if unit.upper()=="k":
+    converted=weight/0.45
+    print("weight in Lbs: " +converted)
+else:
+    converted = weight * 0.45
+    print("weight in kgs: " + str(converted))
+
